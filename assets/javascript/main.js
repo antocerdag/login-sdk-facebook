f9138037031dc9fc2fbc204f948e49165b30fb48
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-05 11:42:55 -0400

    segundo commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,23 @@
-main.js
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1342194079224925',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();   
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
